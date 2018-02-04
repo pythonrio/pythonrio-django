e9be2464b577aa93aca2a8c029e855ea67628346
Luciano Ratamero <luciano@ratamero.com>
2018-02-04 18:01:47 -0200

    view de index só envia como evento os eventos futuros.

--- core/views.py
+++ core/views.py
@@ -1,18 +1,17 @@
+from datetime import datetime
 from django.shortcuts import render
 
 from core.models import Evento
 
 
 def index(request):
-    eventos = Evento.objects.all()
+    eventos = Evento.objects.filter(data__gte=datetime.now())
     return render(request, 'index.html', {'eventos': eventos})
 
 
 def eventos(request):
-    eventos = Evento.objects.all()
     evento_atual = Evento.objects.latest('id')
     context = {
-        'eventos': eventos,
         'evento_atual': evento_atual
     }
     return render(request, 'eventos.html', context)
