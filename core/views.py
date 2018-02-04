30c512f7efce1d7021193a48fc880db2d4b9a23e
Luciano Ratamero <luciano@ratamero.com>
2018-02-04 14:42:16 -0200

    adiciona página vazia de eventos.

--- core/views.py
+++ core/views.py
@@ -6,3 +6,13 @@ from core.models import Evento
 def index(request):
     eventos = Evento.objects.all()
     return render(request, 'index.html', {'eventos': eventos})
+
+
+def eventos(request):
+    eventos = Evento.objects.all()
+    evento_atual = Evento.objects.latest('id')
+    context = {
+        'eventos': eventos,
+        'evento_atual': evento_atual
+    }
+    return render(request, 'eventos.html', context)
