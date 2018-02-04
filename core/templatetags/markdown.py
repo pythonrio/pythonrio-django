42badab9ec7dcb8ebdcd4405ea5272f2198c8a10
Luciano Ratamero <luciano@ratamero.com>
2018-02-04 16:50:29 -0200

    adiciona templatetag pra adicionar classe ao parent gerando pelo markdown.

--- core/templatetags/markdown.py
+++ core/templatetags/markdown.py
@@ -1,3 +1,5 @@
+import re
+from django.utils.safestring import mark_safe
 from markdownx.utils import markdownify
 from django import template
 
@@ -8,3 +10,19 @@ register = template.Library()
 @register.filter
 def render_markdown(value):
     return markdownify(value)
+
+
+@register.filter
+def add_class(value, css_class):
+    class_re = re.compile(r'(?<=class=["\'])(.*)(?=["\'])')
+    string = value
+    match = class_re.search(string)
+    if match:
+        m = re.search(r'^%s$|^%s\s|\s%s\s|\s%s$' % (css_class, css_class,
+                                                    css_class, css_class), match.group(1))
+        if not m:
+            return mark_safe(class_re.sub(match.group(1) + " " + css_class,
+                                          string, 1))
+    else:
+        return mark_safe(string.replace('>', ' class="%s">' % css_class, 1))
+    return value
